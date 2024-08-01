f585f74ccfb17125d88206c8935e71cae9fee0e3
RuchiSingh7095 <124230934+RuchiSingh7095@users.noreply.github.com>
2024-08-01 11:19:59 +0000

    inventory application

---
+++ app.js
@@ -0,0 +1,61 @@
+const createError = require("http-errors");
+const express = require("express");
+const engine = require("ejs-locals");
+const path = require("path");
+const cookieParser = require('cookie-parser');
+const logger = require("morgan");
+
+const inventoryRouter = require("./routes/inventory");
+const compression = require("compression");
+const helmet = require("helmet");
+
+const app = express();
+app.use(helmet());
+
+//Set up mongoose connection
+const mongoose = require("mongoose");
+const dev_db_url = "mongodb://localhost:27017/InventoryDB";
+const mongoDB = process.env.PORT || dev_db_url;
+
+mongoose
+  .connect(dev_db_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Server is running on port 3000"))
+  .catch((err) => console.log(err));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// use ejs-locals for all ejs templates:
+app.engine("ejs", engine);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Compress all routes
+app.use(compression()); 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", inventoryRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
